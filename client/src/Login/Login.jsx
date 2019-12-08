4c813d92874997d54d3506701575446a7c7f7ce9
lordviet <viet1231@abv.bg>
2019-12-08 04:09:32 +0200

    refactored forms to classes

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -2,25 +2,58 @@ import React from 'react';
 import '../shared/styles/login-register.css';
 import { Link as ReactRouterDomLink } from 'react-router-dom';
 
-function Login() {
-    return <div className="login">
-        <img src="marvelAvengers.png" alt="Marvel Avengers" />
-        <form className="loginForm">
-            <h2>Member Login</h2>
-            <div className="form-input">
-                <input type="text" placeholder="Username" autoFocus />
-            </div>
-            <div className="form-input">
-                <input type="password" placeholder="Password" />
-            </div>
-            <div className="form-input">
-                <button type="button">Login</button>
-            </div>
-            <p>Don't have an account?
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+    handleInputChange = (event) => {
+        const { value, name } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        alert('Authentication coming soon!');
+    }
+    render() {
+        return <div className="login">
+            <img src="marvelAvengers.png" alt="Marvel Avengers" />
+            <form onSubmit={this.onSubmit} className="loginForm">
+                <h2>Member Login</h2>
+                <div className="form-input">
+                    <input
+                        name="username"
+                        type="text"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        required
+                        autoFocus />
+                </div>
+                <div className="form-input">
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        required />
+                </div>
+                <div className="form-input">
+                    <button type="submit">Login</button>
+                </div>
+                <p>Don't have an account?
                 <ReactRouterDomLink className="login-register-link" to="/register">Sign up</ReactRouterDomLink>
-            </p>
-        </form>
-    </div>
+                </p>
+            </form>
+        </div>
+    }
 }
 
 export default Login;
--- client/src/Register/Register.jsx
+++ client/src/Register/Register.jsx
@@ -2,28 +2,70 @@ import React from 'react';
 import '../shared/styles/login-register.css';
 import { Link as ReactRouterDomLink } from 'react-router-dom';
 
-function Register() {
-    return <div className="register">
-        <img src="deadpool.jpg" alt="Marvel Characters" />
-        <form className="registerForm">
-            <h2>Member Register</h2>
-            <div className="form-input">
-                <input type="text" placeholder="Username" autoFocus />
-            </div>
-            <div className="form-input">
-                <input type="password" placeholder="Password" />
-            </div>
-            <div className="form-input">
-                <input type="password" placeholder="Repeat Password" />
-            </div>
-            <div className="form-input">
-                <button type="button">Register</button>
-            </div>
-            <p>Already have an account?
+class Register extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            rePassword: ''
+        }
+    }
+
+    handleInputChange = (event) => {
+        const { value, name } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        alert('Authentication coming soon!');
+    }
+
+    render() {
+        return <div className="register">
+            <img src="deadpool.jpg" alt="Marvel Characters" />
+            <form onSubmit={this.onSubmit} className="registerForm">
+                <h2>Member Register</h2>
+                <div className="form-input">
+                    <input
+                        name="username"
+                        type="text"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        required
+                        autoFocus />
+                </div>
+                <div className="form-input">
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        required />
+                </div>
+                <div className="form-input">
+                <input
+                        name="rePassword"
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.rePassword}
+                        onChange={this.handleInputChange}
+                        required />
+                </div>
+                <div className="form-input">
+                    <button type="button">Register</button>
+                </div>
+                <p>Already have an account?
                 <ReactRouterDomLink className="login-register-link" to="/login">Sign in</ReactRouterDomLink>
-            </p>
-        </form>
-    </div>
+                </p>
+            </form>
+        </div>
+    }
 }
 
 export default Register;
