0e07dd755b4ef8ee2f891d96eeb263eaca44274a
lordviet <viet1231@abv.bg>
2019-12-11 20:26:29 +0200

    renamed component

--- client/src/shared/SearchBar/Comics/ComicsList/ComicsList.jsx
+++ client/src/shared/SearchBar/Comics/ComicsList/ComicsList.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './comics.css';
-import marvelAPI from '../../../services/marvel-api';
-import Comic from './Comic/Comic';
-import Loader from '../../Loader/Loader';
+import './comics-list.css';
+import marvelAPI from '../../../../services/marvel-api';
+import Comic from '../Comic/Comic';
+import Loader from '../../../Loader/Loader';
 
 
 class ComicsList extends React.Component {
