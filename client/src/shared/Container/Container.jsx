d41cee44005e4fc9f8f6beb1e84d365b85cac608
lordviet <viet1231@abv.bg>
2019-12-11 20:28:37 +0200

    abstraction for container lists

--- client/src/shared/Container/Container.jsx
+++ client/src/shared/Container/Container.jsx
@@ -1,47 +1,23 @@
 import React from 'react';
 import './container.css';
-import { Link } from 'react-router-dom';
 import Image from '../Image/Image';
+import Available from '../Available/Available';
+import ContainerList from '../Container/ContainerList/ContainerList';
 
 function Container(info) {
-    console.log(info);
-    let characterIds = {};
-    if (info.characters) info.characters.items.forEach(el => characterIds[el.name] = el.resourceURI.split('/').pop());
-
     let creatorIds = {};
     if (info.creators) info.creators.items.forEach(el => creatorIds[el.name] = el.resourceURI.split('/').pop());
 
     return <div className="details">
         <Image {...info.thumbnail} />
         <div className="details-info">
-            <h2>{info.title}</h2>
+            {info.title ? <h2>{info.title}</h2> : null}
+            {info.fullName ? <h2>{info.fullName}</h2> : null}
             {info.details ? <p>{info.details}</p> : null}
-            {info.characters ?
-                <div className="container-list">
-                    <h3>Characters:</h3>
-                    {info.characters.items.map(character =>
-                        <Link className="container-item" to={{
-                            pathname: `/characters/${characterIds[character.name]}`,
-                            state: { characterId: characterIds[character.name] }
-                        }}
-                            key={characterIds[character.name]}>
-                            {character.name}</Link>)}
-                </div>
-                : null
-            }
-            {info.creators ?
-                <div className="container-list">
-                    <h3>Creators: </h3>
-                    {info.creators.items.map(creator =>
-                        <Link className="container-item" to={{
-                            pathname: `/creators/${creatorIds[creator.name]}`,
-                            state: { creatorId: creatorIds[creator.name] }
-                        }}
-                            key={creatorIds[creator.name]}>
-                            {creator.name} ({creator.role})</Link>)}
-                </div>
-                : null
-            }
+            {info.characters ? <ContainerList items={info.characters.items} heading="Characters"/> : null}
+            {info.creators ? <ContainerList items={info.creators.items} heading="Creators"/> : null}
+            {info.comics ? <Available {...info} /> : null}
+            {info.comics ? <ContainerList items={info.comics.items} heading="Comics"/> : null}
             <div className="additional-links">
                 {/* eslint-disable-next-line react/jsx-no-target-blank */}
                 {info.urls[1] ? <a className="linkButton" href={info.urls[1].url} target="_blank">Purchase</a> : null}
---
+++ client/src/shared/Container/ContainerList/ContainerList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './container-list.css';
+import { Link } from 'react-router-dom';
+
+function ContainerList(props) {
+    const items = props.items;
+    let ids = {};
+    items.forEach(el => ids[el.name] = el.resourceURI.split('/').pop());
+
+    return <div className="container-list">
+        <h3>{props.heading}</h3>
+        {items.map(el =>
+            <Link className="container-item" to={{
+                pathname: `/${props.heading.toLowerCase()}/${ids[el.name]}`,
+                state: { id: ids[el.name] }
+            }}
+                key={ids[el.name]}>
+                {el.name} {el.role ? `(${el.role})` : null}</Link>)}
+    </div>
+}
+
+export default ContainerList;
