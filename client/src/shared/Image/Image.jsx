8fd25ccfe98aa4d4414a06017629d411b00f761e
lordviet <viet1231@abv.bg>
2019-12-12 02:14:53 +0200

    conditional image styling

--- client/src/shared/Image/Image.jsx
+++ client/src/shared/Image/Image.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './image.css'
 
-function Image(thumbnail) {
-    const image = thumbnail.path + '.' + thumbnail.extension;
-    return <img src={image} alt="" />
+function Image(props) {
+    const image = props.path + '.' + props.extension;
+    const characterClassName = props.isCharacter ? 'characterImg' : '';
+    return <img src={image} className={characterClassName} alt="" />
 }
 
 export default Image;
